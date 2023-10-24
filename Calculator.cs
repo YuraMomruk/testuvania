f2f6a55adaff1cdb682bd698a718c50d0ea98c1a
YuraMomruk <112652253+YuraMomruk@users.noreply.github.com>
2023-10-24 14:31:36 +0300

    lab1

---
+++ Calculator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Yura
+{
+    public class Calculator
+    {
+        private const char SYMBOL_CLOSE_BRACKET = ')';
+        private const char SYMBOL_OPEN_BRACKET = '(';
+        private const char SYMBOL_OPERATOR_ADD = '+';
+        private const char SYMBOL_OPERATOR_SUB = '-';
+        private const char SYMBOL_OPERATOR_DIV = '/';
+        private const char SYMBOL_OPERATOR_MUL = '*';
+        private const char SYMBOL_OPERATOR_MOD = '%';
+        private const char SYMBOL_UNARY_PLUS = 'p';
+        private const char SYMBOL_UNARY_MINUS = 'm';
+
+        static void Main(string[] args)
+        {
+        }
+
+        private static string InsertSymbol(string input, char symbol, int position)
+        {
+            string res = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == position) res += symbol;
+                else res += input[i];
+            }
+            return res;
+        }
+
+        private static readonly char[] _operators = new char[]  // бінарні операції
+            {
+                SYMBOL_OPERATOR_ADD,
+                SYMBOL_OPERATOR_SUB,
+                SYMBOL_OPERATOR_MUL,
+                SYMBOL_OPERATOR_DIV,
+                SYMBOL_OPERATOR_MOD
+            };
+
+        public static string ReplaceUnaryPlusMinus(string input)
+        {
+            string res = input.Replace(" ", "");
+
+            for (int i = 0; i < res.Length; i++)
+            {
+                char currentSymbol = res[i];
+                if (currentSymbol == SYMBOL_OPERATOR_ADD)
+                {
+                    char previosSymbol = SYMBOL_OPERATOR_MUL;
+
+                    if (i > 0) previosSymbol = res[i - 1];
+
+                    if (i < res.Length - 1)
+                    {
+                        char nextSymbol = res[i + 1];
+                        if ((nextSymbol == SYMBOL_OPEN_BRACKET || char.IsDigit(nextSymbol)) && (_operators.Contains(previosSymbol) || previosSymbol == SYMBOL_OPEN_BRACKET))
+                            res = InsertSymbol(res, SYMBOL_UNARY_PLUS, i);
+                    }
+                }
+
+                if (currentSymbol == SYMBOL_OPERATOR_SUB)
+                {
+                    char previosSymbol = SYMBOL_OPERATOR_MUL;
+
+                    if (i > 0) previosSymbol = res[i - 1];
+
+                    if (i < res.Length - 1)
+                    {
+                        char nextSymbol = res[i + 1];
+                        if ((nextSymbol == SYMBOL_OPEN_BRACKET || char.IsDigit(nextSymbol)) && (_operators.Contains(previosSymbol) || previosSymbol == SYMBOL_OPEN_BRACKET))
+                            res = InsertSymbol(res, SYMBOL_UNARY_MINUS, i);
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ TestReplaceUnaryPlusMinus.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Lab1Yura;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace TestReplaceUnaryPlusMinus
+{
+    [TestClass]
+    public class TestReplaceUnaryPlusMinus
+    {
+        private static IEnumerable<object[]> TestData()
+        {
+            try
+            {
+                XElement xmlData = XElement.Load("TestData.xml");
+
+                var testCases = from testCase in xmlData.Elements("TestCase")
+                                select new object[]
+                                {
+                                    GetElementValueOrNull(testCase.Element("Input")),
+                                    GetElementValueOrNull(testCase.Element("ExpectedResult"))
+                                };
+
+                return testCases;
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Failed to load test data from XML. Exception: {ex.Message}");
+                return Enumerable.Empty<object[]>();
+            }
+        }
+
+        private static string GetElementValueOrNull(XElement element)
+        {
+            return element != null && !string.IsNullOrWhiteSpace(element.Value) ? element.Value : null;
+        }
+
+
+
+
+        [TestMethod]
+        [DynamicData(nameof(TestData), DynamicDataSourceType.Method)]
+        public void ReplaceUnaryPlusMinus_WithXmlData_ShouldReturnExpectedResult(string input, string expectedResult)
+        {
+            if (input == "null")
+            {
+                input = null;
+            }
+            if (expectedResult == "null")
+            {
+                expectedResult = null;
+            }
+            try
+            {
+                // Act
+                string result = Calculator.ReplaceUnaryPlusMinus(input);
+
+                // Assert
+                Assert.AreEqual(expectedResult, result);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Test failed for input '{input}' with exception: {ex.Message}");
+            }
+        }
+    }
+}
